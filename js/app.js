9775c1e46a70c36b0ef76c8ced215630f100609e
akozlova <akozlova200ok@gmail.com>
2019-04-11 10:38:24 -0700

    add functionality for matching cards and closing them if they don't match

--- js/app.js
+++ js/app.js
@@ -10,6 +10,10 @@ function addOpenAndShow(card) {
   card.classList.add("open", "show");
 }
 
+function cardsMatched(card) {
+  card.classList.add('match');
+}
+
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const randomNum = Math.floor(Math.random() * (i + 1));
@@ -33,27 +37,35 @@ for (let i = 0; i < shuffledCards.length; i ++ ) {
 	cardsParent.appendChild(shuffledCards[i]);
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
-//close cards
-allCards.forEach(function(card) {
-  card.addEventListener('click', function(e){
-    removeOpenAndShow(card);
+let selectedCards = [];
+
+shuffledCards.forEach(function(card) {
+  card.addEventListener('click', function(e) {
+
+    selectedCards.push(card);
+
+     if (selectedCards.length <= 2 ) {
+       addOpenAndShow(card);
+
+       if (selectedCards.length === 2) {
+          let firstSelectedCard = selectedCards[0].firstElementChild.className;
+          console.log(firstSelectedCard);
+          let secondSelectedCard = selectedCards[1].firstElementChild.className;
+          console.log(secondSelectedCard);
+          if (firstSelectedCard === secondSelectedCard) {
+            cardsMatched(selectedCards[0]);
+            cardsMatched(selectedCards[1]);
+            } else {
+              console.log('cards did not matched');
+              setTimeout(function() {
+                selectedCards.forEach(function(card) {
+                  removeOpenAndShow(card);
+                })
+              }, 1000);
+          }
+       }
+     }
   });
 });
 
-// open cards
-allCards.forEach(function(card) {
-  card.addEventListener('click', function(e){
-    addOpenAndShow(card);
-  });
-});
